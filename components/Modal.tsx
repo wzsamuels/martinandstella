f117d03f316c836c6b4f00469b447587aea645f3
wzsamuels <wzsamuels@gmail.com>
2023-02-04 00:13:34 -0500

    Added headless-ui and Modal component.

---
+++ components/Modal.tsx
@@ -0,0 +1,62 @@
+import {Dialog, Transition} from "@headlessui/react";
+import {ComponentPropsWithoutRef, Fragment} from "react";
+import {IoMdClose} from "react-icons/io";
+
+interface Props extends ComponentPropsWithoutRef<any> {
+  show: boolean
+  onClose: () => void
+  title: string
+  background?: boolean
+}
+
+const Modal = ({show, onClose, children, title, className, background = true} : Props) => {
+  return (
+    <Transition show={show} as={Fragment}>
+      <Dialog
+        onClose={onClose}
+        className="relative z-50"
+      >
+        {/* The backdrop, rendered as a fixed sibling to the panel container */}
+        {background &&
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        </Transition.Child>
+        }
+        <Transition.Child
+          as={Fragment}
+          enter="transition-all duration-500"
+          enterFrom="top-[1000px]"
+          enterTo="top-0"
+          leave="transition-all duration-500"
+          leaveFrom="bottom-0"
+          leaveTo="top-[1000px]"
+        >
+          <div className="fixed inset-0 flex min-h-full items-center justify-center p-4" style={{overflow: "initial"}}>
+            {/* The actual dialog panel  */}
+            <Dialog.Panel className={`mx-auto w-full max-h-full rounded bg-white flex flex-col ${className}`}>
+              <Dialog.Title className={'flex w-full justify-between sticky top-0 bg-white p-4 text-2xl relative'}>
+                {title}
+                <button className={'rounded-full p-1 hover:bg-gray-100 absolute right-2 top-2'} onClick={onClose}>
+                  <IoMdClose/>
+                </button>
+              </Dialog.Title>
+              <div className={'overflow-y-auto p-4'}>
+                {children}
+              </div>
+            </Dialog.Panel>
+          </div>
+        </Transition.Child>
+      </Dialog>
+    </Transition>
+  )
+}
+
+export default Modal
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "#609f51",
+        lightGreen: "#9fd067"
+      },
+      keyframes: {
+        fromBottom: {
+          'from': {bottom: '-300px', opacity:0},
+          'to': {bottom: 0, opacity:1}
+        },
+        fromLeft: {
+          'from': {left: '-300px', opacity:0},
+          'to': {left:0, opacity:1}
+        },
+        fromRight: {
+          'from': {right: '-300px', opacity:0},
+          'to': {right:0, opacity:1}
+        },
+        fadeIn: {
+          'from': {opacity: 0},
+          'to': {opacity: 1}
+        }
+      }
+    },
   },
   plugins: [],
 }
