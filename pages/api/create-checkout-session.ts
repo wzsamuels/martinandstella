bc4ff99f5adff1eb9760549506a56fb07e3022fa
wzsamuels <wzsamuels@gmail.com>
2023-03-24 23:26:20 -0400

    Add API route for Stripe checkout

---
+++ pages/api/create-checkout-session.ts
@@ -0,0 +1,40 @@
+import { NextApiHandler } from 'next';
+import { stripe } from '../../utils/stripe';
+
+const CreateCheckoutSession: NextApiHandler = async (req, res) => {
+  if (req.method === 'POST') {
+    const { price } = req.body;
+
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        billing_address_collection: 'required',
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product: "prod_NaXQK2F7ihimAx",
+              unit_amount: price * 100
+            },
+            quantity: 1,
+          }
+        ],
+        mode: 'payment',
+        success_url: `${req.headers.origin}/rental?success=true`,
+        cancel_url: `${req.headers.origin}/rental?canceled=true`,
+      });
+
+      return res.status(200).json({ sessionId: session.id });
+    } catch (err: any) {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: { statusCode: 500, message: err.message } });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+};
+
+export default CreateCheckoutSession;
